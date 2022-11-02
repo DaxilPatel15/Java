f881fca559a1c3fc693e32fc51aab7beb4e2085f
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-11-02 15:31:16 -0400

    Create javatester1.java

---
+++ java1/challenge2/javatester1.java
@@ -0,0 +1,32 @@
+public class JavaCodeTester1
+{
+	// Step 2 (main method)
+	public static void main (String [] args)
+	{		
+		// Step 4 (storing a Boolean value)
+		boolean isJavaFun = true;
+		// Step 5 (displaying the boolean value)
+		System.out.print("Step 5 Output (boolean): ");	
+		System.out.println(isJavaFun + "\n");		
+		// Step 6 (storing a character)
+		char nameinitial = 'S';
+		// Step 7 (displaying the character)
+		System.out.print("Step 7 Output (character): ");
+		System.out.println(nameinitial + "\n");
+		// Step 8 (storing two string values)
+		String str1 = "Hello", str2 = "World";
+		// Step 9 (displaying the concatenated value)
+		System.out.print("Step 9 (String Concatenation): ");
+		System.out.println(str1 + str2 + "\n");
+		// Step 10 (storing an integer)
+		int age = 18;
+		// Step 11 (displaying the integer)
+		System.out.print("Step 11 Output (integer): ");
+		System.out.println(age + "\n");
+		// Step 12 (storing a double value)
+		double height = 165.7;
+		// Step 13 (displaying the double value)
+		System.out.print("Step 13 Output (double): ");
+		System.out.println(height + "\n");
+	}
+}
