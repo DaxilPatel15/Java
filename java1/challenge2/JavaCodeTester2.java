8c6a871626131ddeb812d820f5b945763e1328ae
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-11-02 15:32:22 -0400

    Create JavaCodeTester2.java

---
+++ java1/challenge2/JavaCodeTester2.java
@@ -0,0 +1,23 @@
+public class JavaCodeTester2
+{
+
+	public static void main (String [] args)
+	{
+
+		// Step 1 (storing an integer value)
+
+		int age = 18;
+
+		// Step 2 (checks whether the number is odd or even)
+
+		if (age % 2 == 0)
+		{
+			System.out.println(age + "is n even number");
+		}
+
+		else 
+		{
+			System.out.println(age + "is n even number");
+		}
+	
+	}
